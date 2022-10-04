8e336869f71f7f913622342868175aa7de0bd864
Ashraf Soliman <ashraf.8466@gmail.com>
2022-10-04 23:07:11 +0200

    Launch Screen and Welcome Screen

--- nectar/ViewController.swift
+++ nectar/ViewController.swift
@@ -1,19 +1,21 @@
-//
 //  ViewController.swift
 //  nectar
-//
 //  Created by ashraf on 04/10/2022.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var GetStartedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GetStartedButton.layer.cornerRadius = 15
+        GetStartedButton.layer.masksToBounds = true
+
         // Do any additional setup after loading the view.
     }
-
 
 }
 
