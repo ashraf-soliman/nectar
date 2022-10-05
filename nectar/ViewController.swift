64159bea1a1f7d2310f1fd692d83d67dda06bcc4
Ashraf Soliman <ashraf.8466@gmail.com>
2022-10-05 17:59:37 +0200

    Added Scroll View and Table View

--- nectar/ViewController.swift
+++ nectar/ViewController.swift
@@ -7,6 +7,18 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var GetStartedButton: UIButton!
+    @IBOutlet var tableView: UITableView!
+    
+    let options = [
+    "Orders",
+    "My details",
+    "Delivery Address",
+    "Payment Methods",
+    "Promo Code",
+    "Notifications",
+    "Help",
+    "About"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,8 +26,30 @@ class ViewController: UIViewController {
 //        GetStartedButton.layer.cornerRadius = 15
 //        GetStartedButton.layer.masksToBounds = true
 
-        // Do any additional setup after loading the view.
+//        tableView.delegate = self
+//        tableView.dataSource = self
+        
     }
 
 }
 
+//extension ViewController: UITableViewDelegate{
+//
+//}
+
+//extension ViewController: UITableViewDataSource{
+    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = options[indexPath.row]
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return options.count
+//    }
+//}
